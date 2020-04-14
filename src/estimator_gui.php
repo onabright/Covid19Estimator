f0482a7fa426e8785c2fa3f77996cb2fff95ba0c
Bright Onapito <onabright@gmail.com>
2020-04-14 13:51:14 +0300

    made changes to estimator_api_call.php and estimator_gui.php

--- src/estimator_gui.php
+++ src/estimator_gui.php
@@ -54,7 +54,7 @@ $estimate->totalHospitalBeds = $_POST['totalHospitalBeds'];
 	<header>
 		<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
     <div class="container">
-      <a class="navbar-brand js-scroll-trigger" href="#page-top">Covid-19 Estimator</a>
+      <a class="navbar-brand js-scroll-trigger" href="estimator_gui.php">Covid-19 Estimator</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
